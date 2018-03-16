8713d938c96c5c76864e0ae8fdc75a1199ebecfd
Mr Smith <msmith_oz@hotmail.com>
2018-03-16 16:04:58 +1100

    Update

--- Update copy.sh
+++ Update copy.sh
@@ -7,6 +7,15 @@ echo "-------------------"
 echo "Removing '.DS_Store' Files..."
 find . -name '.DS_Store' -delete
 
+echo "Removing 'Packages' File..."
+find . -name 'Packages' -delete
+
+echo "Removing 'Packages.gz' File..."
+find . -name 'Packages.gz' -delete
+
+echo "Removing 'Packages.bz2' File..."
+find . -name 'Packages.bz2' -delete
+
 echo "-------------------"
 echo "Scanning For New Packages..."
 ./dpkg-scanpackages -m ./Files /dev/null >Packages
