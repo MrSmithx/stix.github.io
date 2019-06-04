42884b2bf20fcb0ed2811f17850dc0dc9d21a9da
Mr Smith <msmith_oz@hotmail.com>
2019-06-04 14:19:22 +1000

    Update

--- CleanScan.sh
+++ CleanScan.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 
+rm $BASEDIR/Packages.gz
 rm $BASEDIR/Packages.bz2
 find $BASEDIR -name '.DS_Store' -type f -delete
 
-dpkg-scanpackages -m $BASEDIR/pkg > $BASEDIR/Packages
-bzip2 $BASEDIR/Packages
+dpkg-scanpackages -m $BASEDIR/pkg /dev/null > $BASEDIR/Packages
+gzip -c9 $BASEDIR/Packages > $BASEDIR/Packages.gz
+bzip2 -c9 $BASEDIR/Packages > $BASEDIR/Packages.bz2
